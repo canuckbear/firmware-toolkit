13a0bcc57792967ff4743f4848ef8ccdda464d5d
William BONNET <wbonnet@theitmakers.com>
2016-12-02 19:06:50 +0100

    Handle the different commands, but factory_setup. Entry points are called even if backend is not yet available

---
+++ migrated/migrated/move_me/toolkit/dft/dft/assemble_firmware.py
@@ -0,0 +1,68 @@
+#
+# The contents of this file are subject to the Apache 2.0 license you may not
+# use this file except in compliance with the License.
+#
+# Software distributed under the License is distributed on an "AS IS" basis,
+# WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+# for the specific language governing rights and limitations under the
+# License.
+#
+#
+# Copyright 2016 DFT project (http://www.debianfirmwaretoolkit.org).  
+# All rights reserved. Use is subject to license terms.
+#
+# Debian Firmware Toolkit is the new name of Linux Firmware From Scratch
+# Copyright 2014 LFFS project (http://www.linuxfirmwarefromscratch.org).  
+#
+#
+# Contributors list :
+#
+#    William Bonnet     wllmbnnt@gmail.com, wbonnet@theitmakers.com
+#
+#
+
+import logging, os, subprocess, tarfile, shutil, tempfile, distutils
+from distutils import dir_util, file_util
+from cli_command import CliCommand
+
+#
+#    Class AssembleFirmware
+#
+class AssembleFirmware(CliCommand): 
+    """This class implements method needed to assemble the toolchain used to 
+    assemble firmware inmemory when system is booting.
+
+    Firmware assembling incudes the tasks of generating configuration files 
+    for sqashfs stacking and initramfs generation. It also includes the 
+    creation of the script used to handles reboot to preious version, and
+    integrity check at start.
+    """
+
+    # -------------------------------------------------------------------------
+    #
+    # __init__
+    #
+    # -------------------------------------------------------------------------
+    def __init__(self, dft, project):
+        """Default constructor
+        """
+
+        # Initialize ancestor
+        super().__init__(dft, project)
+
+    # -------------------------------------------------------------------------
+    #
+    # assemble_firmware
+    #
+    # -------------------------------------------------------------------------
+    def assemble_firmware(self):
+        """This method implement the business logic of firmware assembling.
+        
+        TBD
+
+        It calls dedicated method for each step. The main steps are :
+        . 
+        """
+
+        logging.critical("Not yet available")
+        exit(1)
