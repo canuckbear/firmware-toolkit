8fd6a4e0a0e048b6b258fd0c10ab6617d9ffe08d
William BONNET <wbonnet@theitmakers.com>
2017-02-24 19:53:44 +0100

    Minor fixes according topylint

--- migrated/migrated/move_me/toolkit/dft/dft/assemble_firmware.py
+++ migrated/migrated/move_me/toolkit/dft/dft/assemble_firmware.py
@@ -8,11 +8,11 @@
 # License.
 #
 #
-# Copyright 2016 DFT project (http://www.debianfirmwaretoolkit.org).  
+# Copyright 2016 DFT project (http://www.debianfirmwaretoolkit.org).
 # All rights reserved. Use is subject to license terms.
 #
 # Debian Firmware Toolkit is the new name of Linux Firmware From Scratch
-# Copyright 2014 LFFS project (http://www.linuxfirmwarefromscratch.org).  
+# Copyright 2014 LFFS project (http://www.linuxfirmwarefromscratch.org).
 #
 #
 # Contributors list :
@@ -21,19 +21,18 @@
 #
 #
 
-import logging, os, subprocess, tarfile, shutil, tempfile, distutils
-from distutils import dir_util, file_util
+import logging
 from cli_command import CliCommand
 
 #
 #    Class AssembleFirmware
 #
-class AssembleFirmware(CliCommand): 
-    """This class implements method needed to assemble the toolchain used to 
+class AssembleFirmware(CliCommand):
+    """This class implements method needed to assemble the toolchain used to
     assemble firmware inmemory when system is booting.
 
-    Firmware assembling incudes the tasks of generating configuration files 
-    for sqashfs stacking and initramfs generation. It also includes the 
+    Firmware assembling incudes the tasks of generating configuration files
+    for sqashfs stacking and initramfs generation. It also includes the
     creation of the script used to handles reboot to preious version, and
     integrity check at start.
     """
@@ -57,17 +56,15 @@ class AssembleFirmware(CliCommand):
     # -------------------------------------------------------------------------
     def assemble_firmware(self):
         """This method implement the business logic of firmware assembling.
-        
-        Assembling a firmware, use as input the firmware file created from a 
-        baseos and then generate the configuration files used to loading after 
+
+        Assembling a firmware, use as input the firmware file created from a
+        baseos and then generate the configuration files used to loading after
         booting. The configuration is used to define how the filesystems are
         stacked, what should be the physical partitionning, ciphering, etc.
 
         It calls dedicated method for each step. The main steps are :
-        . 
+        .
         """
 
         logging.critical("Not yet available")
         exit(1)
-
-
