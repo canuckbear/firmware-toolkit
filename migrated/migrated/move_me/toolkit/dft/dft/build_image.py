d42fa8a9efb549bb3270893244f64d87b0b3e598
William BONNET <wbonnet@theitmakers.com>
2017-06-14 11:37:26 +0200

    Build image target, WiP

--- migrated/migrated/move_me/toolkit/dft/dft/build_image.py
+++ migrated/migrated/move_me/toolkit/dft/dft/build_image.py
@@ -60,8 +60,137 @@ class BuildImage(CliCommand):
     storage device (harddisk or flash for example). It calls dedicated
     method for each step. The main steps are :
 
-    .
+    . Creating the empty image file
+    . Setup the loopback device
+    . Copy rootfs content to image
+    . Install the boot sectors
+    . Umount the image and release loopback
     """
 
-    logging.critical("Not yet available")
-    exit(1)
+    # Create the image file
+    self.create_image()
+
+    # Create the loopbck device and mount the image file
+    self.setup_loopback()
+
+    # Setup the partitions in the image
+    self.create_partitions()
+
+    # Copy rootfs to the image
+    self.install_image_content()
+
+    # Install the boot (either grub or uboot)
+    self.install_boot()
+
+    # Umount the image and release the loopback deice
+    self.umount_image()
+
+  # -------------------------------------------------------------------------
+  #
+  # create_image
+  #
+  # -------------------------------------------------------------------------
+  def create_image(self):
+    """XXXX This method installs in the generated rootfs the tools needed to update
+    (or generate) theinitramfs. The kernel is not installed, it is the job of
+    the install_bootchain target. The kernel to use is defined in the BSP
+    used by this target.
+
+    Operations executed by this method run in a chrooted environment in the
+    generated rootfs.
+    """
+
+    # Output current task to logs
+    logging.info("Creating the target image file")
+
+  # -------------------------------------------------------------------------
+  #
+  # setup_loopback
+  #
+  # -------------------------------------------------------------------------
+  def setup_loopback(self):
+    """XXXX This method installs in the generated rootfs the tools needed to update
+    (or generate) theinitramfs. The kernel is not installed, it is the job of
+    the install_bootchain target. The kernel to use is defined in the BSP
+    used by this target.
+
+    Operations executed by this method run in a chrooted environment in the
+    generated rootfs.
+    """
+
+    # Output current task to logs
+    logging.info("Setting up the loopback device")
+
+
+  # -------------------------------------------------------------------------
+  #
+  # create_partitions
+  #
+  # -------------------------------------------------------------------------
+  def create_partitions(self):
+    """XXXX This method installs in the generated rootfs the tools needed to update
+    (or generate) theinitramfs. The kernel is not installed, it is the job of
+    the install_bootchain target. The kernel to use is defined in the BSP
+    used by this target.
+
+    Operations executed by this method run in a chrooted environment in the
+    generated rootfs.
+    """
+
+    # Output current task to logs
+    logging.info("Creating the partitions in the image mounted in loopback")
+
+  # -------------------------------------------------------------------------
+  #
+  # install_image_content
+  #
+  # -------------------------------------------------------------------------
+  def install_image_content(self):
+    """XXXX This method installs in the generated rootfs the tools needed to update
+    (or generate) theinitramfs. The kernel is not installed, it is the job of
+    the install_bootchain target. The kernel to use is defined in the BSP
+    used by this target.
+
+    Operations executed by this method run in a chrooted environment in the
+    generated rootfs.
+    """
+
+    # Output current task to logs
+    logging.info("Installating image content")
+
+  # -------------------------------------------------------------------------
+  #
+  # install_boot
+  #
+  # -------------------------------------------------------------------------
+  def install_boot(self):
+    """XXXX This method installs in the generated rootfs the tools needed to update
+    (or generate) theinitramfs. The kernel is not installed, it is the job of
+    the install_bootchain target. The kernel to use is defined in the BSP
+    used by this target.
+
+    Operations executed by this method run in a chrooted environment in the
+    generated rootfs.
+    """
+
+    # Output current task to logs
+    logging.info("Installating the boot (uboot or grub)")
+
+
+  # -------------------------------------------------------------------------
+  #
+  # umount_image
+  #
+  # -------------------------------------------------------------------------
+  def umount_image(self):
+    """XXXX This method installs in the generated rootfs the tools needed to update
+    (or generate) theinitramfs. The kernel is not installed, it is the job of
+    the install_bootchain target. The kernel to use is defined in the BSP
+    used by this target.
+
+    Operations executed by this method run in a chrooted environment in the
+    generated rootfs.
+    """
+
+    # Output current task to logs
+    logging.info("Umounting the image and releasing the loopback devices")
