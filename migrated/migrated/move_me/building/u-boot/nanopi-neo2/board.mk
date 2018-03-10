3b2eb15ab212749c0b5c0922615a744e6ba30c30
William BONNET <wbonnet@theitmakers.com>
2018-03-10 19:40:07 +0100

    Fix u-boot name for nanopi-neo2

--- migrated/migrated/move_me/building/u-boot/nanopi-neo2/board.mk
+++ migrated/migrated/move_me/building/u-boot/nanopi-neo2/board.mk
@@ -28,4 +28,4 @@ USE_CONFIG_FILE        =
 USE_DEFCONFIG          = nanopi_neo2_defconfig
 
 # Defines the list of files to copy (#path is relative to build dir)
-UBOOT_BINARY_FILE      = u-boot-sunxi-with-spl.bin
+UBOOT_BINARY_FILE      = u-boot
