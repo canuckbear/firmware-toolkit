5c100307a2115b123c0ebb2b79e2606da60b8b44
William Bonnet <wbonnet@theitmakers.com>
2018-03-27 23:38:16 +0200

    Fix nitrogen package and add imx6 bords specific packages.

--- migrated/migrated/move_me/building/kernel/nitrogen6-qpmax/board.mk
+++ migrated/migrated/move_me/building/kernel/nitrogen6-qpmax/board.mk
@@ -26,7 +26,7 @@ BOARD_NAME  = nitrogen6-qpmax
 BOARD_ARCH  = armv7l
 
 # Defines the default dtb to use (symlink used by generic boot.scr)
-DEFAULT_DTB = unknown
+DEFAULT_DTB = imx6qp-nitrogen6_max.dtb 
 
 # Defines if all dtb should be included in the generated package (uncomment
 # and set value to 1) or if only default dtb is included (keep commented or
