8e84e45d193cb957e4f556370181d2fa35007887
William Bonnet <wbonnet@theitmakers.com>
2018-05-03 08:27:56 +0200

    Set DTB name for cubie 4

--- migrated/migrated/move_me/building/kernel/cubieboard-4/board.mk
+++ migrated/migrated/move_me/building/kernel/cubieboard-4/board.mk
@@ -26,7 +26,7 @@ BOARD_NAME  = cubieboard-4
 BOARD_ARCH  = armv7l
 
 # Defines the default dtb to use (symlink used by generic boot.scr)
-DEFAULT_DTB = 
+DEFAULT_DTB = sun9i-a80-cubieboard4.dtb
 
 # Defines if all dtb should be included in the generated package (uncomment
 # and set value to 1) or if only default dtb is included (keep commented or
