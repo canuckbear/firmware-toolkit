1662fe17617024c5376abdf9429344183c293597
William Bonnet <william@wbonnet.net>
2019-04-02 21:39:10 +0200

    fix version

--- migrated/migrated/move_me/toolkit/dft/release.py
+++ migrated/migrated/move_me/toolkit/dft/release.py
@@ -29,6 +29,6 @@ This information is intented to be used in the setup.py file, and cli.py (displa
 program version), etc.
 """
 
-__version__ = '0.6.10'
+__version__ = '0.6.11'
 __author__ = 'William Bonnet'
 __author_email__ = 'wbonnet@theitmakers.com'
