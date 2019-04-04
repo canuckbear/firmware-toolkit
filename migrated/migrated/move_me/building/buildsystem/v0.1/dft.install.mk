1dba3e033bbf74f7eb9d136c1f504cc2249bc61a
William Bonnet <william@wbonnet.net>
2019-04-04 18:57:07 +0000

    fix error message

--- migrated/migrated/move_me/building/buildsystem/v0.1/dft.install.mk
+++ migrated/migrated/move_me/building/buildsystem/v0.1/dft.install.mk
@@ -75,37 +75,3 @@ do-reinstall :
 
 
 
-le do installl est fau
- DEPMOD  5.0.3
-make[1]: Leaving directory '/media/william/132c8116-ac9b-49b0-b626-85714ec13422/work/dft/building/kernel/pinebook/5.0.3/work-pinebook/build-pinebook/linux-5.0.3'
-cp: cannot stat 'arch/aarch64/boot/dts/*.dtb': No such file or directory
-    completed [install] 
-    completed [reinstall] 
-(william@pinebook):5.0.3$ ls /media/william/132c8116-ac9b-49b0-b626-85714ec13422/work/dft/building/kernel/pinebook/5.0.3/work-pinebook/build-pinebook/linux-5.0.3
-arch        certs    crypto         firmware  init    Kconfig  LICENSES     mm               Module.symvers  samples   sound       usr      vmlinux.o
-block       COPYING  Documentation  fs        ipc     kernel   MAINTAINERS  modules.builtin  net             scripts   System.map  virt
-built-in.a  CREDITS  drivers        include   Kbuild  lib      Makefile     modules.order    README          security  tools       vmlinux
-(william@pinebook):5.0.3$ ls /media/william/132c8116-ac9b-49b0-b626-85714ec13422/work/dft/building/kernel/pinebook/5.0.3/work-pinebook/build-pinebook/linux-5.0.3/arch/
-alpha  arm    c6x   h8300    ia64     m68k        mips   nios2     parisc   riscv  sh     um         x86
-arc    arm64  csky  hexagon  Kconfig  microblaze  nds32  openrisc  powerpc  s390   sparc  unicore32  xtensa
-(william@pinebook):5.0.3$ ls /media/william/132c8116-ac9b-49b0-b626-85714ec13422/work/dft/building/kernel/pinebook/5.0.3/work-pinebook/build-pinebook/linux-5.0.3/arch/armboot           kvm           mach-berlin     mach-footbridge  mach-keystone  mach-mxs        mach-prima2    mach-sa1100    mach-uniphier   net             tools
-common         lib           mach-clps711x   mach-gemini      mach-ks8695    mach-netx       mach-pxa       mach-shmobile  mach-ux500      nwfpe           vdso
-configs        mach-actions  mach-cns3xxx    mach-highbank    mach-lpc18xx   mach-nomadik    mach-qcom      mach-socfpga   mach-versatile  oprofile        vfp
-crypto         mach-alpine   mach-davinci    mach-hisi        mach-lpc32xx   mach-npcm       mach-rda       mach-spear     mach-vexpress   plat-iop        xen
-firmware       mach-artpec   mach-digicolor  mach-imx         mach-mediatek  mach-nspire     mach-realview  mach-sti       mach-vt8500     plat-omap
-include        mach-asm9260  mach-dove       mach-integrator  mach-meson     mach-omap1      mach-rockchip  mach-stm32     mach-w90x900    plat-orion
-Kconfig        mach-aspeed   mach-ebsa110    mach-iop13xx     mach-mmp       mach-omap2      mach-rpc       mach-sunxi     mach-zx         plat-pxa
-Kconfig.debug  mach-at91     mach-efm32      mach-iop32x      mach-moxart    mach-orion5x    mach-s3c24xx   mach-tango     mach-zynq       plat-samsung
-Kconfig-nommu  mach-axxia    mach-ep93xx     mach-iop33x      mach-mv78xx0   mach-oxnas      mach-s3c64xx   mach-tegra     Makefile        plat-versatile
-kernel         mach-bcm      mach-exynos     mach-ixp4xx      mach-mvebu     mach-picoxcell  mach-s5pv210   mach-u300      mm              probes
-(william@pinebook):5.0.3$ ls /media/william/132c8116-ac9b-49b0-b626-85714ec13422/work/dft/building/kernel/pinebook/5.0.3/work-pinebook/build-pinebook/linux-5.0.3/arch/arm64/
-boot  configs  crypto  include  Kconfig  Kconfig.debug  Kconfig.platforms  kernel  kvm  lib  Makefile  mm  net  xen
-(william@pinebook):5.0.3$ ls /media/william/132c8116-ac9b-49b0-b626-85714ec13422/work/dft/building/kernel/pinebook/5.0.3/work-pinebook/build-pinebook/linux-5.0.3/arch/arm64/boot/
-dts  Image  Image.gz  install.sh  Makefile
-
-
-
-
-tu melange linstall des dtb et du noyau ce sont deux trucs different reprend calmement en regrdant ce que le pinbook a d install dans / boo et procede par analogie
-
-
