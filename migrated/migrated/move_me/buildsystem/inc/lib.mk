b3791093f71f09c77a85114caa06c46deff349a7
William BONNET <wbonnet@theitmakers.com>
2020-04-27 09:26:31 +0200

    add warning_as_error argument and warning macro

--- migrated/migrated/move_me/buildsystem/inc/lib.mk
+++ migrated/migrated/move_me/buildsystem/inc/lib.mk
@@ -50,11 +50,11 @@
 # execution returning with provided error code or 1 as default value
 # TODO Handle default error code
 define dft_error =
-echo "DFT ERROR CODE : $(1)" && exit 1
+echo "DFT ERROR : $(1)" && exit 72
 endef
 
 define dft_error_message =
-echo "DFT ERROR MSG : $(1)"
+echo "DFT ERROR message : $(1)"
 endef
 
 # ------------------------------------------------------------------------------
@@ -62,5 +62,9 @@ endef
 # definition of function dft_warning : display a message and continue execution
 #
 define dft_warning =
-@echo "DFT Warning : $(1)" ;
+echo "DFT warning : $(1)" && [ "$(warning_as_error)" == "1" ] && exit 73
+endef
+
+define dft_warning_message =
+echo "DFT warning message : $(1)"
 endef
