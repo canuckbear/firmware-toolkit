222218160c6687722d2932ab429ff4896fa22e25
William BONNET <wbonnet@theitmakers.com>
2020-05-15 16:31:25 +0000

    remove debug trace

--- migrated/migrated/move_me/buildsystem/inc/target-fetch.mk
+++ migrated/migrated/move_me/buildsystem/inc/target-fetch.mk
@@ -78,18 +78,14 @@ fetch-archive-%:
 		if [ -f $(COOKIE_DIR)/$@ ] ; then \
 			true ; \
 		else \
-		echo "        plop3" ; \
 			wget $(WGET_OPTS) -T 30 -c -P $(PARTIAL_DIR) $(SRC_DIST_URL)/$* ; \
-		echo "        plop4" ; \
 			mv $(PARTIAL_DIR)/$* $(DOWNLOAD_DIR) ; \
 			rmdir --ignore-fail-on-non-empty $(PARTIAL_DIR) ; \
 			if [ -f $(DOWNLOAD_DIR)/$* ] ; then \
 				true ; \
 			else \
-		echo "        plop1" ; \
 				echo 'ERROR : Failed to download $(SRC_DIST_URL)/$*' 1>&2; \
 			        $(call dft_error ,2004-1602); \
-		echo "        plop2" ; \
 			fi ; \
 		fi ; \
 	fi ; 
