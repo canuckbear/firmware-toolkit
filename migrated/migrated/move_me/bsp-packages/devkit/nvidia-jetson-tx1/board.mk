76cb3151e83eb066fb8b34768544c9d2d6fb3103
William BONNET <wbonnet@theitmakers.com>
2020-06-01 06:52:40 +0200

    add missing nvidia prefix to board name

--- migrated/migrated/move_me/bsp-packages/devkit/nvidia-jetson-tx1/board.mk
+++ migrated/migrated/move_me/bsp-packages/devkit/nvidia-jetson-tx1/board.mk
@@ -23,7 +23,7 @@
 #
 
 # The board name fragment will be used in package naming, path and files content generation
-BOARD_NAME = jetson-tx1
+BOARD_NAME = nvidia-jetson-tx1
 
 # Board architecture as defined by uname -m (armv7l, mips, aarch64, x86_64, etc.)
 BOARD_ARCH = aarch64
