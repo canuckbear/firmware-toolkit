a660f49790dff5e06594ab7657b6e402700d1cdc
William BONNET <wbonnet@theitmakers.com>
2020-06-02 12:42:28 +0000

    remove buggy comment

--- migrated/migrated/move_me/buildsystem/inc/target-package.mk
+++ migrated/migrated/move_me/buildsystem/inc/target-package.mk
@@ -48,12 +48,10 @@ SW_VERSION  ?= out-of-scope
 
 do-package:
 	@skip_target=0 ; \
-# Check if make is running at generic level or target to build level \
 	if [ "$(SW_VERSION)" == "out-of-scope" ] ; then \
 		skip_target=1 ; \
 		true ; \
 	fi; \
-\
 	if [ ! "x$(HOST_ARCH)" = "x$(BOARD_ARCH)" ] && [ "x$(only-native-arch)" = "x1" ] ; then \
 		skip_target=1 ; \
 		if [ "x$(arch-warning)" = "x1" ] ; then \
@@ -65,17 +63,13 @@ do-package:
 			echo "In order to generate binaries for existing architectures, you need several builders, one for each target arch." ; \
 		fi ; \
 	fi ; \
-# Check if target should be executed even if current arch is different from target arch \
-# Check if skip flag has been raised it not then do the job \
 	if [ ! "x$$skip_target" = "x1" ] ; then \
 		if [ ! -f $(COOKIE_DIR)/do-package ] ; then \
-# Create the workdir used to generate packaging scripts \
 			echo " DEBUG : Je sauis dans target package" ; \
 			pwd ; \
 			ls -lah ; \
 			mkdir $(PACKAGE_DIR)/debian ; \
 			if [ "$(SW_NAME)" = "u-boot" ] ; then \
-# Test if current package is a u-boot then copy template and expand variables in files copy \
 				cp -fr --dereference $(DFT_BUILDSYSTEM)/templates/debian-u-boot-package/* $(PACKAGE_DIR)/debian ; \
 				mv  $(PACKAGE_DIR)/debian/u-boot.install $(PACKAGE_DIR)/debian/u-boot-$(BOARD_NAME).install ; \
 				mv  $(PACKAGE_DIR)/debian/u-boot.postinst $(PACKAGE_DIR)/debian/u-boot-$(BOARD_NAME).postinst ; \
@@ -84,7 +78,6 @@ do-package:
 				mv  $(PACKAGE_DIR)/debian/u-boot.postrm $(PACKAGE_DIR)/debian/u-boot-$(BOARD_NAME).postrm ; \
 				cp -frv --dereference $(PWD)/files $(PACKAGE_DIR)/doc ; \
 			fi ; \
-# Test if current package is a linux kernel then copy template and expand variables in files copy\
 			if [ "$(SW_NAME)" = "linux" ] ; then \
 				cp -fr --dereference $(DFT_BUILDSYSTEM)/templates/debian-linux-kernel-package/* $(PACKAGE_DIR)/debian ; \
 				mv  $(PACKAGE_DIR)/debian/linux-kernel.install $(PACKAGE_DIR)/debian/linux-kernel-$(BOARD_NAME).install ; \
@@ -93,7 +86,6 @@ do-package:
 				mv  $(PACKAGE_DIR)/debian/linux-kernel.prerm $(PACKAGE_DIR)/debian/linux-kernel-$(BOARD_NAME).prerm ; \
 				mv  $(PACKAGE_DIR)/debian/linux-kernel.postrm $(PACKAGE_DIR)/debian/linux-kernel-$(BOARD_NAME).postrm ; \
 			fi ; \
-# Expand variables in package workdir files copied from template \
 			if [ "$(DEBEMAIL)" = "" ] ; then \
 				find $(PACKAGE_DIR)/debian -type f | xargs sed -i -e "s/__MAINTAINER_EMAIL__/unknown/g" ; \
 			else \
@@ -107,9 +99,7 @@ do-package:
 			find $(PACKAGE_DIR)/debian -type f | xargs sed -i -e "s/__SW_VERSION__/$(SW_VERSION)/g" \
 										  -e "s/__BOARD_NAME__/$(BOARD_NAME)/g" \
 										  -e "s/__DATE__/$(shell LC_ALL=C date +"%a, %d %b %Y %T %z")/g" ; \
-# Copy files to install into package directory (still under work tree) \
 			cp -frv $(INSTALL_DIR)/* $(PACKAGE_DIR) ; \
-# Go into package definition directory then create package source tarball into parent directory\
 			cd $(PACKAGE_DIR) ; \
 			if [ "$(SW_NAME)" = "linux" ] ; then \
 				tar cfz ../$(SW_NAME)-kernel-$(BOARD_NAME)_$(SW_VERSION).orig.tar.gz * ; \
