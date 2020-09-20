c92129b4864d7db092af3c3e824c4e546e255978
William BONNET <wbonnet@theitmakers.com>
2020-09-20 13:29:56 +0000

    fix boardname

--- migrated/migrated/move_me/board-support/single-board-computer/radxa-rockpi-s/board.mk
+++ migrated/migrated/move_me/board-support/single-board-computer/radxa-rockpi-s/board.mk
@@ -24,7 +24,7 @@
 #
 
 # The board name fragment will be used in package naming, path and files content generation
-BOARD_NAME = rockpi-s
+BOARD_NAME = radxa-rockpi-s
 
 # Board architecture as defined by uname -m (armv7l, mips, aarch64, x86_64, etc.)
 BOARD_ARCH = aarch64
