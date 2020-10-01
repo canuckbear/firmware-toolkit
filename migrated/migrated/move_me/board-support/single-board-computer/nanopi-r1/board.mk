c99f2319b12d8bff486f095f39e23d4677ec7b13
William BONNET <wbonnet@theitmakers.com>
2020-10-01 12:30:45 +0000

    use nanopi_r1_defconfig until dedicated defconfig is available

--- migrated/migrated/move_me/board-support/single-board-computer/nanopi-r1/board.mk
+++ migrated/migrated/move_me/board-support/single-board-computer/nanopi-r1/board.mk
@@ -38,10 +38,10 @@ BOARD_ARCH = armv7l
 UBOOT_SUPPORT     = 1
 
 # Make use the defconfig file from UBOOT_DEFCONFIG unless you specify your own in USE_CONFIG_FILE
-UBOOT_DEFCONFIG   = nanopi_r1_defconfig
+UBOOT_DEFCONFIG   = nanopi_m1_plus_defconfig
 
 # List of files to copy from build to install directory (path is relative to build dir)
-UBOOT_BINARY_FILE = u-boot-$(BOARD_NAME)
+UBOOT_BINARY_FILE = u-boot-sunxi-with-spl.bin
 DEFAULT_DTB       = nanopir1.dtb
 
 #
