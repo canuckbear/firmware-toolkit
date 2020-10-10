74eb59f0b5adb23d805bd44a703c20087a6d1246
William BONNET <wbonnet@theitmakers.com>
2020-10-10 22:41:34 +0000

    fix board name

--- migrated/migrated/move_me/board-support/single-board-computer/beagleboard-beaglebone-black/board.mk
+++ migrated/migrated/move_me/board-support/single-board-computer/beagleboard-beaglebone-black/board.mk
@@ -24,7 +24,7 @@
 #
 
 # The board name fragment will be used in package naming, path and files content generation
-BOARD_NAME = beaglebone-black
+BOARD_NAME = beagleboard-beaglebone-black
 
 # Board architecture as defined by uname -m (armv7l, mips, aarch64, x86_64, etc.)
 BOARD_ARCH = armv7l
