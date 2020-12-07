3ae9e33fa08f664a4c903da5151913f1eae51416
William BONNET <wbonnet@theitmakers.com>
2020-12-07 16:28:07 +0000

    fix board name

--- migrated/migrated/move_me/board-support/single-board-computer/raspberry-pi0/board.mk
+++ migrated/migrated/move_me/board-support/single-board-computer/raspberry-pi0/board.mk
@@ -24,7 +24,7 @@
 #
 
 # The board name fragment will be used in package naming, path and files content generation
-BOARD_NAME = orangepi-zero
+BOARD_NAME = raspberry-pi0
 
 # Board architecture as defined by uname -m (armv7l, mips, aarch64, x86_64, etc.)
 BOARD_ARCH = armv7l
