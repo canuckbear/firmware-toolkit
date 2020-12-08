a7969cfd42f8cc0bd80ae892a06382f63ede0778
William BONNET <wbonnet@theitmakers.com>
2020-12-08 18:33:48 +0000

    fix board arch synonym was misused

--- migrated/migrated/move_me/board-support/single-board-computer/nanopi-m1-plus/board.mk
+++ migrated/migrated/move_me/board-support/single-board-computer/nanopi-m1-plus/board.mk
@@ -27,7 +27,7 @@
 BOARD_NAME = nanopi-m1-plus
 
 # Board architecture as defined by uname -m (armv7l, mips, aarch64, x86_64, etc.)
-BOARD_ARCH = armhf
+BOARD_ARCH = armv7l
 
 #
 # u-boot support and configuration
