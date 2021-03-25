a25e21bdeb331db7bce0b0bb8bf5a8fb75160451
William BONNET <wbonnet@theitmakers.com>
2021-03-25 00:14:32 +0000

    add idbloader.img in package

--- migrated/migrated/move_me/board-support/single-board-computer/asus-tinkerboard/board.mk
+++ migrated/migrated/move_me/board-support/single-board-computer/asus-tinkerboard/board.mk
@@ -49,7 +49,7 @@ UBOOT_DEFCONFIG = tinker-rk3288_defconfig
 
 # List of files to copy from build to install directory (path is relative to build dir)
 UBOOT_BINARY_FILE = u-boot-$(BOARD_NAME)
-UBOOT_BINARY_EXTRA_FILES = u-boot-dtb.img 
+UBOOT_BINARY_EXTRA_FILES = idbloader.img 
 DEFAULT_DTB = rk3288-tinker.dtb
 
 
