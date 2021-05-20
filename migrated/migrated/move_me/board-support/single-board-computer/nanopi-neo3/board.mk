6a449361bae9263c44b8471c72db70da1a12ace5
William BONNET <wbonnet@theitmakers.com>
2021-05-20 20:24:30 +0200

    fix neo3 uboot defconfif, so fas r2s file is used

--- migrated/migrated/move_me/board-support/single-board-computer/nanopi-neo3/board.mk
+++ migrated/migrated/move_me/board-support/single-board-computer/nanopi-neo3/board.mk
@@ -49,8 +49,12 @@ UBOOT_DEFCONFIG = nanopi-r2s-rk3328_defconfig
 UBOOT_BINARY_FILE = u-boot.itb
 >>>>>>> 53b854946 (fix uboot binary name for nanopi neo3)
 UBOOT_BINARY_EXTRA_FILES = idbloader.img  
+<<<<<<< HEAD
 UBOOT_BINARY_FILE = u-boot-nanopi-neo3
 DEFAULT_DTB = nanopi-neo3.dtb
+=======
+DEFAULT_DTB = rk3328-nanopi-r2s.dtb
+>>>>>>> 3d751ed44 (fix neo3 uboot defconfif, so fas r2s file is used)
 
 
 #
