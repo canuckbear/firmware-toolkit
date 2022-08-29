14991655ed693c322bca97b93cd7e2731fe4922d
William BONNET <william@wbonnet.net>
2022-08-29 23:48:27 +0200

    Fix default firmwaretoolkit repo sub path

--- dft-cli/dft/cli_command.py
+++ dft-cli/dft/cli_command.py
@@ -622,7 +622,7 @@ class CliCommand(object):
           filepath += "firmwaretoolkit_repository.list"
 #          working_file.write("deb http://apt.firmaretoolkit.org/debian " + version)
 #          working_file.write(" main firmwaretoolkit\n")
-          working_file.write("deb [trusted=yes] http://192.168.11.128/armwizard/armwizard " + version + " bullseye main debian armwizard ") 
+          working_file.write("deb [trusted=yes] http://192.168.11.128/firmwaretoolkit " + version + " debian firmwaretoolkit/mainline\n") 
 
           # Check if the public key of the repository is defined in the BSP file, otherwise
           # Set the default value of the key
