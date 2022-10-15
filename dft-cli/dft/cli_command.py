394e1070097727a00b4da23c85e0cbb04c7ed5a3
William BONNET <william@wbonnet.net>
2022-10-15 20:20:17 +0200

    fix typo in firmwaretoolkit repo source list

--- dft-cli/dft/cli_command.py
+++ dft-cli/dft/cli_command.py
@@ -621,7 +621,7 @@ class CliCommand(object):
           # Defines the file name and content for firmware-toolkit APT sources
           logging.debug("Using firmwaretoolkit repo as source provider. Adding firmwaretoolkit.list")
           filepath += "firmwaretoolkit_repository.list"
-          working_file.write("deb https://apt.firmwaretoolkit.org/ " + version + " debian stable lonterm mainline\n") 
+          working_file.write("deb https://apt.firmwaretoolkit.org/ " + version + " debian stable longterm mainline\n") 
 
           # Check if the public key of the repository is defined in the BSP file, otherwise
           # Set the default value of the key
